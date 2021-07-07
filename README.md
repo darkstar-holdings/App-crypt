704be35027afd04d1dd93a0e69abf14c31ce24d7
David Betz <hashref@gmail.com>
2021-07-07 21:55:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::crypt - It's new $module
+
+# SYNOPSIS
+
+    use App::crypt;
+
+# DESCRIPTION
+
+App::crypt is ...
+
+# LICENSE
+
+Copyright (C) David Betz.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+David Betz <hashref@gmail.com>
---
+++ lib/App/crypt.pm
@@ -0,0 +1,39 @@
+package App::crypt;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::crypt - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::crypt;
+
+=head1 DESCRIPTION
+
+App::crypt is ...
+
+=head1 LICENSE
+
+Copyright (C) David Betz.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+David Betz E<lt>hashref@gmail.comE<gt>
+
+=cut
+
